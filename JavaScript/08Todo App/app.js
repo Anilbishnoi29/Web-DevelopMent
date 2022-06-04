9e8164a74e8c1c27335b38b07fcdd7e3a13414c5
Anilbishnoi29 <bishnoianilakb@gmail.com>
2022-06-04 07:37:42 +0530

    Todo App

--- JavaScript/08Todo App/app.js
+++ JavaScript/08Todo App/app.js
@@ -1,28 +1,31 @@
 const addNewTaskCategoryBtn = document.getElementById("addNewTaskCategoryBtn");
 // new Category
 
-let flag = false;
+let flag = true;
 
 addNewTaskCategoryBtn.addEventListener('click', function () {
     const addNewTaskCategory = document.getElementById("addNewTaskCategory");
     const categoryUl = document.getElementById("categoryUl");
     const categoryName = addNewTaskCategory.value;
-    const categoryLi = document.createElement("li");
-    categoryLi.classList = "categoryLi";
-    categoryLi.innerText = categoryName;
-    categoryUl.appendChild(categoryLi);
-    addNewTaskCategory.value = "";
-    if (flag == false) {
-        flag = true;
-    };
+    if (categoryName.length <= 20 && categoryName.length >= 5) {
+        const categoryLi = document.createElement("li");
+        categoryLi.classList = "categoryLi";
+        categoryLi.innerText = categoryName;
+        categoryUl.appendChild(categoryLi);
+        addNewTaskCategory.value = "";
+        // addStyleAtFirstLi();
+        addLiStyle();
+    } else {
+        alert(`length must be 5 char to 20 char.`);
+        console.log(categoryName.length);
+        addNewTaskCategory.value = "";
+    }
 });
 const categoryLi = document.querySelector("#categoryUl");
 
 
 
-
-
-function addToTaskBar(e) {
+function addLiStyle(e) {
     let li = document.getElementsByClassName('categoryLi');
     for (let i = 0; i < li.length; i++) {
         li[i].onclick = function () {
@@ -36,16 +39,38 @@ function addToTaskBar(e) {
                 n = n.nextSibling;
             }
             this.classList = 'categoryLi activeTaskList';
+            // e.classList = 'categoryLi activeTaskList';
         }
     }
-    const liName = e.target;
+}
 
+function addToTaskBar(e) {
+    addLiStyle();
+    const liName = e.target;
     const liValue = liName.innerText;
-    let bgColor = liName.css;
-    console.log(bgColor);
+    const styles = getComputedStyle(liName)
+    const bgColor = styles.backgroundColor;
+    console.log(`bgColor : ${bgColor}`);
 
     const allTaskTitle = document.querySelector("#ulTitle");
     allTaskTitle.innerText = liValue;
+    allTaskTitleParent = allTaskTitle.parentElement;
+    allTaskParent = allTaskTitleParent.parentElement;
+    allTaskParent.parentElement.style.border = `5px solid ${bgColor}`;
     // console.log(liValue)
 }
+
+// function addStyleAtFirstLi() {
+//     if (flag) {
+//         console.log("Hello");
+//         let li = document.getElementsByClassName('categoryLi');
+//         for (let i = 0; i < li.length; i++) {
+//             li[i].onclick = function () {
+//                 let n = li[0];
+//                 n.classList = 'categoryLi activeTaskList';
+//             }
+//             flag = false;
+//         }
+//     };
+// }
 categoryLi.addEventListener('click', addToTaskBar);
