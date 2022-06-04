391fb30a45fded7732e4ad38aba8a99f13bd1d30
Anilbishnoi29 <bishnoianilakb@gmail.com>
2022-06-04 10:50:06 +0530

    Todo App

--- JavaScript/08Todo App/app.js
+++ JavaScript/08Todo App/app.js
@@ -1,76 +1,77 @@
-const addNewTaskCategoryBtn = document.getElementById("addNewTaskCategoryBtn");
-// new Category
+'use strick';
+const categoryUl = document.querySelector('#categoryUl');
+const categoryBtn = document.querySelector('#addNewTaskCategoryBtn');
 
-let flag = true;
 
-addNewTaskCategoryBtn.addEventListener('click', function () {
-    const addNewTaskCategory = document.getElementById("addNewTaskCategory");
-    const categoryUl = document.getElementById("categoryUl");
-    const categoryName = addNewTaskCategory.value;
-    if (categoryName.length <= 20 && categoryName.length >= 5) {
-        const categoryLi = document.createElement("li");
-        categoryLi.classList = "categoryLi";
-        categoryLi.innerText = categoryName;
-        categoryUl.appendChild(categoryLi);
-        addNewTaskCategory.value = "";
-        // addStyleAtFirstLi();
-        addLiStyle();
+const LOCAL_STORAGE_LIST_KEY = 'task.list';
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
+
+renderCategory();
+categoryBtn.addEventListener('click', e => {
+    e.preventDefault;
+    const categoryName = document.querySelector('#addNewTaskCategory').value;
+    if (categoryName.length <= 20 && categoryName.length >= 3) {
+        const categoryList = createList(categoryName);
+        lists.push(categoryList);
+        saveAndRender();
     } else {
-        alert(`length must be 5 char to 20 char.`);
-        console.log(categoryName.length);
-        addNewTaskCategory.value = "";
+        alert(`length must be 3 char to 20 char.`);
     }
-});
-const categoryLi = document.querySelector("#categoryUl");
+    document.querySelector('#addNewTaskCategory').value = "";
+})
 
+function saveAndRender() {
+    saveTaskCategory();
+    renderCategory();
+}
+// save task in local storage
 
-
-function addLiStyle(e) {
-    let li = document.getElementsByClassName('categoryLi');
-    for (let i = 0; i < li.length; i++) {
-        li[i].onclick = function () {
-            let n = li[0];
-            console.log(n);
-            while (n) {
-                if (n.classList[1] === 'activeTaskList') {
-                    console.log(n);
-                    n.classList.remove('activeTaskList');
-                }
-                n = n.nextSibling;
-            }
-            this.classList = 'categoryLi activeTaskList';
-            // e.classList = 'categoryLi activeTaskList';
+function saveTaskCategory() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+}
+// create list obj
+function createList(name) {
+    return {
+        id: Date.now().toString(),
+        name: name,
+        tasksList: []
+    }
+}
+// 
+categoryUl.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'li') {
+        selectedListId = e.target.dataset.listId;
+        saveAndRender();
+    }
+})
+// append all category in li
+function renderCategory() {
+    clearCategoryList(categoryUl);
+    lists.forEach(list => {
+        const categoryLi = document.createElement("li"); // create a new li
+        categoryLi.dataset.listId = list.id;
+        categoryLi.classList = "categoryLi"; // add a class that li
+        let innerLi = `<button id="trashOfUl"><i class="fas fa-solid fa-trash"></i></button>`
+        // categoryLi.innerText = list.name; // set name of task list
+        categoryLi.innerHTML = `${list.name} ${innerLi}`; // set name of task list
+        if (list.id === selectedListId) {
+            categoryLi.classList.add('activeTaskList');
+            const trashOfUl = categoryLi.lastChild;
+            trashOfUl.addEventListener('click', () => {
+                lists = lists.filter(list => list.id !== selectedListId)
+                selectedListId = null;
+                saveAndRender();
+            })
         }
+        categoryUl.appendChild(categoryLi); // append li in ul
+    })
+}
+console.log(lists);
+
+function clearCategoryList(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
     }
 }
-
-function addToTaskBar(e) {
-    addLiStyle();
-    const liName = e.target;
-    const liValue = liName.innerText;
-    const styles = getComputedStyle(liName)
-    const bgColor = styles.backgroundColor;
-    console.log(`bgColor : ${bgColor}`);
-
-    const allTaskTitle = document.querySelector("#ulTitle");
-    allTaskTitle.innerText = liValue;
-    allTaskTitleParent = allTaskTitle.parentElement;
-    allTaskParent = allTaskTitleParent.parentElement;
-    allTaskParent.parentElement.style.border = `5px solid ${bgColor}`;
-    // console.log(liValue)
-}
-
-// function addStyleAtFirstLi() {
-//     if (flag) {
-//         console.log("Hello");
-//         let li = document.getElementsByClassName('categoryLi');
-//         for (let i = 0; i < li.length; i++) {
-//             li[i].onclick = function () {
-//                 let n = li[0];
-//                 n.classList = 'categoryLi activeTaskList';
-//             }
-//             flag = false;
-//         }
-//     };
-// }
-categoryLi.addEventListener('click', addToTaskBar);
