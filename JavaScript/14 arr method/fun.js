ed7e4f7e750a47edf0d17ea37037ae34c3ec2907
Anilbishnoi29 <bishnoianilakb@gmail.com>
2022-06-25 21:07:56 +0530

    update

---
+++ JavaScript/14 arr method/fun.js
@@ -0,0 +1,13 @@
+'use strict';
+let y = function fun() {
+    console.log("book reading");
+}
+
+
+function x(y) {
+    console.log("x");
+}
+
+setTimeout(function x(y) {
+    alert("hello")
+}, 5000);
